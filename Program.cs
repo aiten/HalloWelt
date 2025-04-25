8947fb841f8d85c7960198215e4f7407cbce80ba
Herbert Aitenbichler <h.aitenbichler@eduhi.at>
2025-04-25 10:21:51 +0200

    Add more messages

--- Program.cs
+++ Program.cs
@@ -1,3 +1,19 @@
 ﻿Console.WriteLine("Hello, World!");
 Console.WriteLine("1CHIF!");
 Console.WriteLine("2024/2025");
+
+
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine("1CHIF!");
+Console.WriteLine("2024/2025");
+Console.WriteLine("Hello, World!");
+Console.WriteLine("1CHIF!");
+Console.WriteLine("2024/2025");
+Console.WriteLine("Hello, World!");
+Console.WriteLine("1CHIF!");
+Console.WriteLine("2024/2025");
+Console.WriteLine("Hello, World!");
+Console.WriteLine("1CHIF!");
+Console.WriteLine("2024/2025");
+
